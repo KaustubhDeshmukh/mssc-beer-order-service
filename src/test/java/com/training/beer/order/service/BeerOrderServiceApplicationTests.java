b0972c2ef0b931f2cd0b353a29d360937b738771
kaustubh <kaustubh.deshmukh@bluejeans.com>
2021-09-14 19:13:10 -0700

    Added local mysql config, renmaed packages to follow com.training convention

---
+++ src/test/java/com/training/beer/order/service/BeerOrderServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.training.beer.order.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("mysql")
+public class BeerOrderServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
